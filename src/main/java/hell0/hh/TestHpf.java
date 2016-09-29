0c2683499152e98639104445a1f55578134c5425
panfeng.hou <hpf99@126.com>
2016-09-29 11:33:47 +0800

    testhpf

---
+++ src/main/java/hell0/hh/TestHpf.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 1999-2004 Alibaba.com All right reserved. This software is the
+ * confidential and proprietary information of Alibaba.com ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Alibaba.com.
+ */
+package hell0.hh;
+
+/**
+ * 类TestHpf.java的实现描述：TODO 类实现描述 
+ * @author panfeng.hou 2016年9月29日 上午11:33:26
+ */
+public class TestHpf {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
