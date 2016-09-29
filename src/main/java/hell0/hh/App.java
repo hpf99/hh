44616f03c446f73e30e89fe08b1b77aab11135cc
panfeng.hou <hpf99@126.com>
2016-09-29 11:27:02 +0800

    testhh

---
+++ src/main/java/hell0/hh/App.java
@@ -0,0 +1,13 @@
+package hell0.hh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
